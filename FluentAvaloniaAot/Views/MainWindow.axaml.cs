74379a62c73fa41e048eadf2dcbd02cb9d9de1b3
吴俊 <8400684@qq.com>
2023-02-17 01:26:24 +0800

    添加项目文件。

---
+++ FluentAvaloniaAot/Views/MainWindow.axaml.cs
@@ -0,0 +1,175 @@
+using System;
+using System.Runtime.InteropServices;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using Avalonia.Media.Immutable;
+using Avalonia.Styling;
+using FluentAvalonia.Styling;
+using FluentAvalonia.UI.Controls;
+using FluentAvalonia.UI.Media;
+using FluentAvalonia.UI.Windowing;
+
+namespace FluentAvaloniaAot.Views;
+
+public partial class MainWindow : AppWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        //SplashScreen = new SampleAppSplashScreen();
+#if DEBUG
+        this.AttachDevTools();
+#endif
+        MinWidth = 450;
+        MinHeight = 400;
+
+        TitleBar.ExtendsContentIntoTitleBar = true;
+        TitleBar.TitleBarHitTestType = TitleBarHitTestType.Complex;
+
+        Application.Current.ActualThemeVariantChanged += ApplicationActualThemeVariantChanged;
+    }
+
+    private void ApplicationActualThemeVariantChanged(object sender, EventArgs e)
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            // TODO: add Windows version to CoreWindow
+            if (IsWindows11 && ActualThemeVariant != FluentAvaloniaTheme.HighContrastTheme)
+            {
+                TryEnableMicaEffect();
+            }
+            else if (ActualThemeVariant != FluentAvaloniaTheme.HighContrastTheme)
+            {
+                // Clear the local value here, and let the normal styles take over for HighContrast theme
+                SetValue(BackgroundProperty, AvaloniaProperty.UnsetValue);
+            }
+        }
+    }
+
+    protected override void OnOpened(EventArgs e)
+    {
+        base.OnOpened(e);
+
+        var thm = ActualThemeVariant;
+
+        // Enable Mica on Windows 11
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            // TODO: add Windows version to CoreWindow
+            if (IsWindows11 && thm != FluentAvaloniaTheme.HighContrastTheme)
+            {
+                TransparencyBackgroundFallback = Brushes.Transparent;
+                TransparencyLevelHint = WindowTransparencyLevel.Mica;
+
+                TryEnableMicaEffect();
+            }
+        }
+
+        var screen = Screens.ScreenFromVisual(this);
+        if (screen != null)
+        {
+            double width = Width;
+            double height = Height;
+
+            if (screen.WorkingArea.Width > 1280)
+            {
+                width = 1280;
+            }
+            else if (screen.WorkingArea.Width > 1000)
+            {
+                width = 1000;
+            }
+            else if (screen.WorkingArea.Width > 700)
+            {
+                width = 700;
+            }
+            else if (screen.WorkingArea.Width > 500)
+            {
+                width = 500;
+            }
+            else
+            {
+                width = 450;
+            }
+
+            if (screen.WorkingArea.Height > 720)
+            {
+                width = 720;
+            }
+            else if (screen.WorkingArea.Height > 600)
+            {
+                width = 600;
+            }
+            else if (screen.WorkingArea.Height > 500)
+            {
+                width = 500;
+            }
+            else
+            {
+                width = 400;
+            }
+        }
+    }
+
+    private void TryEnableMicaEffect()
+    {
+        // The background colors for the Mica brush are still based around SolidBackgroundFillColorBase resource
+        // BUT since we can't control the actual Mica brush color, we have to use the window background to create
+        // the same effect. However, we can't use SolidBackgroundFillColorBase directly since its opaque, and if
+        // we set the opacity the color become lighter than we want. So we take the normal color, darken it and 
+        // apply the opacity until we get the roughly the correct color
+        // NOTE that the effect still doesn't look right, but it suffices. Ideally we need access to the Mica
+        // CompositionBrush to properly change the color but I don't know if we can do that or not
+        if (ActualThemeVariant == ThemeVariant.Dark)
+        {
+            var color = this.TryFindResource("SolidBackgroundFillColorBase",
+                ThemeVariant.Dark, out var value)
+                ? (Color2)(Color)value
+                : new Color2(32, 32, 32);
+
+            color = color.LightenPercent(-0.8f);
+
+            Background = new ImmutableSolidColorBrush(color, 0.78);
+        }
+        else if (ActualThemeVariant == ThemeVariant.Light)
+        {
+            // Similar effect here
+            var color = this.TryFindResource("SolidBackgroundFillColorBase",
+                ThemeVariant.Light, out var value)
+                ? (Color2)(Color)value
+                : new Color2(243, 243, 243);
+
+            color = color.LightenPercent(0.5f);
+
+            Background = new ImmutableSolidColorBrush(color, 0.9);
+        }
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+    private TaskDialogProgressState _progressFlags = TaskDialogProgressState.Indeterminate;
+    async void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var td = new TaskDialog
+        {
+            Title = "ssss",
+            Header = "ssss",
+            SubHeader ="ssss",
+            Content = "ssss",
+            ShowProgressBar = true,
+            FooterVisibility = TaskDialogFooterVisibility.Always,
+            IsFooterExpanded = true,
+            Footer = new CheckBox { Content = "Never show me this again" }
+        };
+        
+        td.SetProgressBarState(50, _progressFlags);
+        
+        td.XamlRoot = VisualRoot as Visual;
+        var result = await td.ShowAsync(this.FindControl<CheckBox>("ShowWindowedCheck").IsChecked == false);
+    }
+}
